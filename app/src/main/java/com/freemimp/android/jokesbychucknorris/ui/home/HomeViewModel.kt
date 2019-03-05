b2d508e7f5c4518fd22028d46721e73b8c19db93
Egons Zile <egons.zile@royalmail.com>
2019-03-05 17:23:10 +0000

    initial commit - add dependencies, add home fragment and homeviewmodel, create basic home screen ui with 3 buttons

---
+++ app/src/main/java/com/freemimp/android/jokesbychucknorris/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.freemimp.android.jokesbychucknorris.ui.home
+
+import android.arch.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/freemimp/android/jokesbychucknorris/utils/Extensions.kt
@@ -0,0 +1,2 @@
+package com.freemimp.android.jokesbychucknorris.utils
+
